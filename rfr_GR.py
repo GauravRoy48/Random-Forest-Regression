1069ec0240af9fcb2990ae70bc653f3f38969a05
Gaurav Roy <gauravroy0408@gmail.com>
2019-05-13 01:35:46 +0530

    Added all files

---
+++ rfr_GR.py
@@ -0,0 +1,35 @@
+##################################################################################
+# Creator     : Gaurav Roy
+# Date        : 13 May 2019
+# Description : The code contains the template for Random Forest Regression on  
+#               the Position_Salaries.csv.
+##################################################################################
+
+# Importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import Dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:2].values
+Y = dataset.iloc[:,2:3].values
+
+# Fitting Decision Tree Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X, Y)
+
+# Predicting a new result with Decision Tree Regression
+Y_pred =regressor.predict(np.array([[6.5]]))
+
+# Visualizing Decision Tree Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X, Y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Decision Tree Regression Results')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.grid()
+plt.show()
